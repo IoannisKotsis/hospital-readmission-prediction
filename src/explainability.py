18e877be8d75fea913b4e55d790054a5f94f87e2
IoannisKotsis <john01ko18@gmail.com>
2026-06-21 14:57:20 +0300

    Resize SHAP values image

--- src/explainability.py
+++ src/explainability.py
@@ -14,7 +14,7 @@ def shap_explainer(model, X_test):
     shap_values = explainer.shap_values(X_test)
 
     #visualization
-    shap.summary_plot(shap_values, X_test, plot_type="dot", show = False, plot_size = (8,6))
+    shap.summary_plot(shap_values, X_test, plot_type="dot", show = False, plot_size = (6,4))
     plt.savefig('shap_values.png', bbox_inches = 'tight', pad_inches = 0.3)
     plt.close()
 
